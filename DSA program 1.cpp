95c99a6972a5fc032ed8f37bdf8ce09370b8ea30
Ath-arv <83336617+Ath-arv@users.noreply.github.com>
2021-09-05 12:03:06 +0530

    Add files via upload

---
+++ DSA program 1.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main() {
+	char *subjects[7]={"Discrete Structures","Data Communication","DSA","Cloud Computing",
+	"App development using java","Unix & Shell Modelling","Web Development"};
+	
+	cout<<subjects[0]<<"\n";
+	cout<<subjects[1]<<"\n";
+	cout<<subjects[2]<<"\n";
+	cout<<subjects[3]<<"\n";
+	cout<<subjects[4]<<"\n";
+	cout<<subjects[5]<<"\n";
+	cout<<subjects[6]<<"\n";
+	
+	return 0;
+}
+
