90030a1bedb1bbb08cfd404e5d3be6fef5a71ac4
Ath-arv <83336617+Ath-arv@users.noreply.github.com>
2021-10-20 11:45:17 +0530

    Update 5_element_linked_list.cpp

--- 5_element_linked_list.cpp
+++ 5_element_linked_list.cpp
@@ -1,36 +1,41 @@
 #include<iostream>
-#include<malloc.h>
+#include<stdlib.h>
 using namespace std;
 struct Node
 {
 	int data;
-	struct Node *next;
+	struct Node *link;
 	
-}*FIRST=NULL;
+};
 int main()
 { struct Node *n1,*n2,*n3,*n4,*n5;
 n1=(Node*)malloc(sizeof(struct Node));
 n1->data=5;
-n1->next=NULL;
-FIRST=n1;
+n1->link=NULL;
+
 n2=(Node*)malloc(sizeof(struct Node));
 n2->data=4;
-n1->next=n2;
+n2->link=NULL;
+n1->link=n2;
+
 n3=(Node*)malloc(sizeof(struct Node));
 n3->data=3;
-n2->next=n3;
+n3->link=NULL;
+n2->link=n3;
 n4=(Node*)malloc(sizeof(struct Node));
 n4->data=2;
-n3->next=n4;
+n4->link=NULL;
+n3->link=n4;
 n5=(Node*)malloc(sizeof(struct Node));
 n5->data=1;
-n4->next=n5;
+n5->link=NULL;
+n4->link=n5;
 cout<<"the existing linked list";
-struct Node *temp=FIRST;
+struct Node *temp=n1;
 cout<<"\n ";
 while(temp!=NULL)
 {cout<<temp->data<<"==>";
-temp=temp->next;
+temp=temp->link;
 }
 return 0;
 }
